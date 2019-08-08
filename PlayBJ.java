cd851f858804d0990ec4755d885ab194406bd352
ksmdmi5 <52361352+ksmdmi5@users.noreply.github.com>
2019-08-08 08:27:29 -0600

    Delete PlayBJ.java

--- PlayBJ.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.cards.blackjack;
-
-	public interface PlayBJ {
-
-		public void checkHand(BlackjackHand playerhand);
-
-		boolean startHand(BlackjackHand playerhand);
-}
-
